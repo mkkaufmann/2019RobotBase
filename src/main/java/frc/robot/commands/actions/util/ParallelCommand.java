eebcbecc7633d77dba643816912bc6e8ae59394b
mkkaufmann <mkaufmann421@gmail.com>
2019-04-17 18:17:34 -0400

    list as parameter for sequential and parallel commands

--- src/main/java/frc/robot/commands/actions/util/ParallelCommand.java
+++ src/main/java/frc/robot/commands/actions/util/ParallelCommand.java
@@ -1,6 +1,9 @@
 package frc.robot.commands.actions.util;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.commands.NoCommand;
+
+import java.util.List;
 
 public class ParallelCommand extends Command {
 
@@ -10,6 +13,22 @@ public class ParallelCommand extends Command {
     public ParallelCommand(Command a, Command b){
         this.a = a;
         this.b = b;
+    }
+
+    public ParallelCommand(List<Command> list){
+        if(list.isEmpty()){
+            this.a = new NoCommand();
+            this.b = new NoCommand();
+        }else if(list.size() == 1){
+            this.a = list.get(0);
+            this.b = new NoCommand();
+        }else if(list.size() == 2){
+            this.a = list.get(0);
+            this.b = list.get(1);
+        }else{
+            this.a = list.remove(0);
+            this.b = new ParallelCommand(list);
+        }
     }
 
     @Override
--- src/main/java/frc/robot/commands/actions/util/SequentialCommand.java
+++ src/main/java/frc/robot/commands/actions/util/SequentialCommand.java
@@ -1,6 +1,9 @@
 package frc.robot.commands.actions.util;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.commands.NoCommand;
+
+import java.util.List;
 
 public class SequentialCommand extends Command {
 
@@ -10,6 +13,22 @@ public class SequentialCommand extends Command {
     public SequentialCommand(Command a, Command b){
         this.a = a;
         this.b = b;
+    }
+
+    public SequentialCommand(List<Command> list){
+        if(list.isEmpty()){
+            this.a = new NoCommand();
+            this.b = new NoCommand();
+        }else if(list.size() == 1){
+            this.a = list.get(0);
+            this.b = new NoCommand();
+        }else if(list.size() == 2){
+            this.a = list.get(0);
+            this.b = list.get(1);
+        }else{
+            this.a = list.remove(0);
+            this.b = new SequentialCommand(list);
+        }
     }
 
     @Override
